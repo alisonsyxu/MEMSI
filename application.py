b2ec8ee306f3e1453385fa566e37dbfa980dbc1c
Suyan Xu <suyan.xu.1@gmail.com>
2020-02-02 18:47:42 +0800

    fgd

--- application.py
+++ application.py
@@ -28,6 +28,18 @@ def get_data():
     #         }
     #     }
 
-    testdata = 'test data'
+    testdata = {
+        "data":{
+            "dataSets": [{
+                "values": [{"x": 1, "y": 88},{"x": 2, "y": 77},{"x": 3, "y": 105},{"x": 4, "y": 135},{"x": 5, "y": 88},{"x": 6, "y": 77},{"x": 7, "y": 105},{"x": 8, "y": 135}],
+                "label": 'A',
+            }, {
+                "values": [{"x": 1, "y": 90},{"x": 2, "y": 130},{"x": 3, "y": 100},{"x": 4, "y": 105},{"x": 5, "y": 90},{"x": 6, "y": 130},{"x": 7, "y": 100},{"x": 8, "y": 105}],
+                "label": 'B',
+            }],
+            }
+        }
+
+    # testdata = 'test data'
 
     return jsonify(testdata)
